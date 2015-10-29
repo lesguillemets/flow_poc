4c3a4db9142667e22269979e78069c2efdfa7344
lesguillemets <lesguillemets@users.noreply.github.com>
2015-10-29 11:34:56 +0900

    add withalpha helper

--- src/Helper.hs
+++ src/Helper.hs
@@ -1,6 +1,7 @@
 module Helper (
     smul
   , vadd , (<+>)
+  , withAlpha
               ) where
 
 import Haste.Graphics.Canvas
@@ -14,3 +15,7 @@ vadd (x,y) (z,w) = (x+z, y+w)
 (<+>) :: Vector -> Vector -> Vector
 (<+>) = vadd
 infixl 6 <+>
+
+withAlpha :: Color -> Double -> Color
+withAlpha c@(RGBA{}) _ = c
+withAlpha (RGB r g b) a = RGBA r g b a
